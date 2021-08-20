819a06a3280945db3680a185894dc62cb68f645a
jungho <jungho@juvis.co.kr>
2021-08-20 15:28:41 +0900

    init commit

---
+++ src/main/java/kr/co/thirtytwodevs/namingboxserver/NamingBoxServerApplication.java
@@ -0,0 +1,13 @@
+package kr.co.thirtytwodevs.namingboxserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingBoxServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamingBoxServerApplication.class, args);
+    }
+
+}
